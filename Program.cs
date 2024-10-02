6e8278afe86e02599ecb60d2a7147ac515cb4393
Alenta <mef082024@gmail.com>
2024-10-02 10:46:02 +0200

    Init

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Panagram;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //string str = "abcdefghijklmnopqrstuvwxyz";
+        //Console.WriteLine($"Is {str} a pangram? "+ IsPangram(str));
+    }
+
+    public static bool IsPangram(string str)
+    {
+        for (int i = 0; i < 26; i++)
+        {
+            char c = Convert.ToChar(i + 97); 
+            if(!str.Contains(c)) return false;
+        }
+        return true;
+    }
+
+    
+}
